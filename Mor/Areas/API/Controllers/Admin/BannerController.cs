db0a9d5d755c9991bfb5d813daf6f0f2dfaf4fa9
rogue <634428127qq.com>
2017-09-28 02:32:29 +0800

    新增banner

---
+++ Mor/Areas/API/Controllers/Admin/BannerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mor.Areas.API.Controllers
+{
+    public class BannerController : Controller
+    {
+        //
+        // GET: /API/Banner/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
