951f2af0166c0a30ed119e9acbc46ec8607ee699
zhizd <zhdenglife@qq.com>
2017-09-27 21:39:43 +0800

    change1

--- Mor/App_Start/RouteConfig.cs
+++ Mor/App_Start/RouteConfig.cs
@@ -20,7 +20,7 @@ namespace Mor
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Index", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
